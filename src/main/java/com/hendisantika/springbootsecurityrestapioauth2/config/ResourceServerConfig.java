db1fa8b8593c2d570cc4ec9c29dc90accbf08bea
Hendi Santika <hendisantika@gmail.com>
2020-02-08 06:54:22 +0700

    #21 - Add ResourceServerConfig

---
+++ src/main/java/com/hendisantika/springbootsecurityrestapioauth2/config/ResourceServerConfig.java
@@ -0,0 +1,19 @@
+package com.hendisantika.springbootsecurityrestapioauth2.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-security-restapi-oauth2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/02/20
+ * Time: 06.53
+ */
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+}
