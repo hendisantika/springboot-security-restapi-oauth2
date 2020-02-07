c38060a848268498891db1cf72a611efc5ef63cf
Hendi Santika <hendisantika@gmail.com>
2020-02-08 06:48:16 +0700

    #9 - Add UserServiceImpl

---
+++ src/main/java/com/hendisantika/springbootsecurityrestapioauth2/service/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springbootsecurityrestapioauth2.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-security-restapi-oauth2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/02/20
+ * Time: 06.48
+ */
+@Service("userService")
+public class UserServiceImpl implements UserService {
+}
