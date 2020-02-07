1d80de4eb9d3ddf39b190b722a598fc05c159e59
Hendi Santika <hendisantika@gmail.com>
2020-02-08 06:51:40 +0700

    #14 - Add AuthorizationServerConfig

---
+++ src/main/java/com/hendisantika/springbootsecurityrestapioauth2/config/AuthorizationServerConfig.java
@@ -0,0 +1,19 @@
+package com.hendisantika.springbootsecurityrestapioauth2.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-security-restapi-oauth2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/02/20
+ * Time: 06.51
+ */
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+}
