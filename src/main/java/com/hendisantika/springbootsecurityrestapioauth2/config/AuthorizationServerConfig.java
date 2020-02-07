120cf2c3fc2e8032ba5cb26204b2ddd40e541321
Hendi Santika <hendisantika@gmail.com>
2020-02-08 06:52:05 +0700

    #15 - Add many fields in AuthorizationServerConfig

--- src/main/java/com/hendisantika/springbootsecurityrestapioauth2/config/AuthorizationServerConfig.java
+++ src/main/java/com/hendisantika/springbootsecurityrestapioauth2/config/AuthorizationServerConfig.java
@@ -1,8 +1,14 @@
 package com.hendisantika.springbootsecurityrestapioauth2.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.provider.approval.UserApprovalHandler;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,4 +22,23 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+    @Value("${oauth.realm}")
+    private String realm;
+
+    @Value("${oauth.clientId}")
+    private String clientId;
+
+    @Value("${oauth.secret}")
+    private String secret;
+
+    @Autowired
+    private TokenStore tokenStore;
+
+    @Autowired
+    private UserApprovalHandler userApprovalHandler;
+
+    @Autowired
+    @Qualifier("authenticationManagerBean")
+    private AuthenticationManager authenticationManager;
+
 }
