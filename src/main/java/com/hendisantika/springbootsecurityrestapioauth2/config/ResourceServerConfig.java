07d246daf63e1f9c7d56586da8425fff69dd0e90
Hendi Santika <hendisantika@gmail.com>
2020-02-08 06:55:11 +0700

    #24 - Add configure #2 in ResourceServerConfig

--- src/main/java/com/hendisantika/springbootsecurityrestapioauth2/config/ResourceServerConfig.java
+++ src/main/java/com/hendisantika/springbootsecurityrestapioauth2/config/ResourceServerConfig.java
@@ -2,10 +2,12 @@ package com.hendisantika.springbootsecurityrestapioauth2.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,5 +28,21 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 
     public void configure(ResourceServerSecurityConfigurer resources) {
         resources.resourceId(resourceId).stateless(false);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        http
+                .csrf().disable()
+                .anonymous().disable()
+                .authorizeRequests()
+                .antMatchers("/oauth/token").permitAll()
+                .and()
+                .requestMatchers().antMatchers("/user/**")
+                .and().authorizeRequests()
+                .antMatchers("/user/**").access("hasRole('ADMIN')")
+                .and().exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
+
     }
 }
