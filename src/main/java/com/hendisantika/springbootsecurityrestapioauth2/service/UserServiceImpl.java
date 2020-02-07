b6ad6ea8c08b1527092bc814125134fd67fe6ca3
Hendi Santika <hendisantika@gmail.com>
2020-02-08 06:48:49 +0700

    #10 - Add many fields in UserServiceImpl

--- src/main/java/com/hendisantika/springbootsecurityrestapioauth2/service/UserServiceImpl.java
+++ src/main/java/com/hendisantika/springbootsecurityrestapioauth2/service/UserServiceImpl.java
@@ -1,6 +1,10 @@
 package com.hendisantika.springbootsecurityrestapioauth2.service;
 
+import com.hendisantika.springbootsecurityrestapioauth2.model.User;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,4 +17,12 @@ import org.springframework.stereotype.Service;
  */
 @Service("userService")
 public class UserServiceImpl implements UserService {
+    private static final AtomicLong counter = new AtomicLong();
+
+    private static List<User> users;
+
+    static {
+        users = getDummyUsers();
+    }
+
 }
