15310bcf38b437113e7a501d2cfe03d52051f6b4
Hendi Santika <hendisantika@gmail.com>
2020-02-08 06:47:20 +0700

    #7 - Add SpringBootServletInitializer in Main Class

--- src/main/java/com/hendisantika/springbootsecurityrestapioauth2/SpringbootSecurityRestapiOauth2Application.java
+++ src/main/java/com/hendisantika/springbootsecurityrestapioauth2/SpringbootSecurityRestapiOauth2Application.java
@@ -2,9 +2,16 @@ package com.hendisantika.springbootsecurityrestapioauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootSecurityRestapiOauth2Application {
+public class SpringbootSecurityRestapiOauth2Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootSecurityRestapiOauth2Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootSecurityRestapiOauth2Application.class, args);
