61e932d1349da97a16178e54dad33cf50ec9cea4
Hendi Santika <hendisantika@gmail.com>
2020-02-09 08:27:01 +0700

    #34 - Add many fields in RestClient

--- src/main/java/com/hendisantika/springbootsecurityrestapioauth2/controller/RestClient.java
+++ src/main/java/com/hendisantika/springbootsecurityrestapioauth2/controller/RestClient.java
@@ -10,4 +10,12 @@ package com.hendisantika.springbootsecurityrestapioauth2.controller;
  * Time: 08.26
  */
 public class RestClient {
+    public static final String REST_SERVICE_URI = "http://localhost:8080/Spring-Boot-Security-RestAPI-OAuth2";
+
+    public static final String AUTH_SERVER_URI = "http://localhost:8080/Spring-Boot-Security-RestAPI-OAuth2/oauth" +
+            "/token";
+
+    public static final String QPM_PASSWORD_GRANT = "?grant_type=password&username=henry&password=abc123";
+
+    public static final String QPM_ACCESS_TOKEN = "?access_token=";
 }
