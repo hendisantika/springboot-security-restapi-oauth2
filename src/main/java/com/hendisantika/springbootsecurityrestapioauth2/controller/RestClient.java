d416f86fafc62d8f3124c032c996bdb18e75dd79
Hendi Santika <hendisantika@gmail.com>
2020-02-09 08:26:21 +0700

    #33 - Add RestClient

---
+++ src/main/java/com/hendisantika/springbootsecurityrestapioauth2/controller/RestClient.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootsecurityrestapioauth2.controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-security-restapi-oauth2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/02/20
+ * Time: 08.26
+ */
+public class RestClient {
+}
