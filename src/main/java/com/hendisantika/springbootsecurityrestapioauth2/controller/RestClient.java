dffeb70fde0a32268ad79e1d2ea20ae20bb72bc1
Hendi Santika <hendisantika@gmail.com>
2020-02-09 08:28:20 +0700

    #37 - Add sendTokenRequest in RestClient

--- src/main/java/com/hendisantika/springbootsecurityrestapioauth2/controller/RestClient.java
+++ src/main/java/com/hendisantika/springbootsecurityrestapioauth2/controller/RestClient.java
@@ -1,10 +1,16 @@
 package com.hendisantika.springbootsecurityrestapioauth2.controller;
 
+import com.hendisantika.springbootsecurityrestapioauth2.model.AuthTokenInfo;
 import org.apache.commons.codec.binary.Base64;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 
 /**
  * Created by IntelliJ IDEA.
@@ -44,5 +50,34 @@ public class RestClient {
         HttpHeaders headers = getHeaders();
         headers.add("Authorization", "Basic " + base64ClientCredentials);
         return headers;
+    }
+
+    /*
+     * Send a POST request [on /oauth/token] to get an access-token, which will then be send with each request.
+     */
+    @SuppressWarnings({"unchecked"})
+    private static AuthTokenInfo sendTokenRequest() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpEntity<String> request = new HttpEntity<String>(getHeadersWithClientCredentials());
+        ResponseEntity<Object> response = restTemplate.exchange(AUTH_SERVER_URI + QPM_PASSWORD_GRANT, HttpMethod.POST
+                , request, Object.class);
+        LinkedHashMap<String, Object> map = (LinkedHashMap<String, Object>) response.getBody();
+        AuthTokenInfo tokenInfo = null;
+
+        if (map != null) {
+            tokenInfo = new AuthTokenInfo();
+            tokenInfo.setAccessToken((String) map.get("access_token"));
+            tokenInfo.setTokenType((String) map.get("token_type"));
+            tokenInfo.setRefreshToken((String) map.get("refresh_token"));
+            tokenInfo.setExpiresIn((int) map.get("expires_in"));
+            tokenInfo.setScope((String) map.get("scope"));
+            System.out.println(tokenInfo);
+
+        } else {
+            System.out.println("No user exist----------");
+
+        }
+        return tokenInfo;
     }
 }
